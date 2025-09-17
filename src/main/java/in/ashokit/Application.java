f0124a723b0e50b9e56e12d0c7ea7f6066b2acfa
Ankit <tumhara_github_email@example.com>
2025-09-17 19:28:01 +0530

    JIRA - 102 complited

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+		// JIRA - 102
+		public void m2(){
+		// logic 
+}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
              int j = 10;
